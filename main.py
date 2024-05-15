f12b7de3280fe2254b392303b270eeb573565eb9
Ícar Nin Solana <icar.nin@protonmail.com>
2024-05-15 21:45:08 +0200

    wip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
 import sys
+
 import gi
 
 gi.require_version('Gtuber', '0.0')
@@ -17,19 +17,21 @@ try:
 except GLib.Error as err:
     print(err.message)
 
-if info:
-    print("TITLE: {}".format(info.props.title))
-    print("DURATION: {}".format(info.props.duration))
+if not info:
+    raise Exception("Could not find media info")
 
-    streams = info.get_streams()
-    adaptive_streams = info.get_adaptive_streams()
+print("TITLE: {}".format(info.props.title))
+print("DURATION: {}".format(info.props.duration))
 
-    print("STREAMS: {}".format(len(streams)))
-    print("ADAPTIVE STREAMS: {}\n".format(len(adaptive_streams)))
+streams = info.get_streams()
+adaptive_streams = info.get_adaptive_streams()
 
-    for arr in streams, adaptive_streams:
-        for stream in arr:
-            print("VIDEO CODEC: {}".format(stream.props.video_codec))
-            print("AUDIO CODEC: {}".format(stream.props.audio_codec))
-            print("RESOLUTION: {}x{}@{}".format(stream.props.width, stream.props.height, stream.props.fps))
-            print("URI: {}\n".format(stream.props.uri))
+print("STREAMS: {}".format(len(streams)))
+print("ADAPTIVE STREAMS: {}\n".format(len(adaptive_streams)))
+
+for arr in streams, adaptive_streams:
+    for stream in arr:
+        print("VIDEO CODEC: {}".format(stream.props.video_codec))
+        print("AUDIO CODEC: {}".format(stream.props.audio_codec))
+        print("RESOLUTION: {}x{}@{}".format(stream.props.width, stream.props.height, stream.props.fps))
+        print("URI: {}\n".format(stream.props.uri))
